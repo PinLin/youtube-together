f47f0ac408b35e0ce3578aae1bff7c9d743bfcc9
Pin Lin <pinlin945@gmail.com>
2021-09-12 22:27:56 +0800

    Sync la

--- src/app.ts
+++ src/app.ts
@@ -18,10 +18,47 @@ app.get('/hello', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server);
 
+interface Room {
+    media: string;
+    state: number;
+    speed: number;
+    progress: number;
+    updateTime?: number;
+}
+const rooms = {} as { [userId: string]: Room };
+
 io.on('connection', (socket) => {
     console.log(`User ${socket.id} connected.`);
 
+    socket.on('host', () => {
+        function update() {
+            var startTime = Date.now();
+            socket.emit('update', (room: Room) => {
+                var offset = (Date.now() - startTime) / 2000;
+                rooms[socket.id] = {
+                    ...room,
+                    progress: room.progress + offset,
+                    updateTime: Date.now(),
+                };
+            });
+
+            setTimeout(() => { update(); }, 1000);
+        }
+        update();
+    });
+
+    socket.on('sync', (userId: string, callback: (room: Room) => void) => {
+        const room = rooms[userId];
+        if (room) {
+            callback({
+                ...room,
+                progress: room.progress + (Date.now() - room.updateTime) / 1000,
+            });
+        }
+    });
+
     socket.on('disconnect', () => {
+        delete rooms[socket.id];
         console.log(`User ${socket.id} disconnected.`);
     });
 });
